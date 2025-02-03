8556b515d3966b3b6fd070e91f04652759b8c831
DucBui99-code <bduc25300@gmail.com>
2025-02-03 16:41:47 +0700

    Update cors allow all device

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/database");
 const errorHandler = require("./middlewares/errorHandler");
 const requestLogger = require("./middlewares/requestLogger");
 const rateLimiter = require("./middlewares/rateLimiter");
+const cors = require("cors");
 
 dotenv.config({ path: "./config.env" });
 
@@ -12,9 +13,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 app.use(requestLogger);
 app.use(rateLimiter);
-
 app.use(errorHandler);
 
 app.use("/v1/MovieApp", require("./routes/index"));
